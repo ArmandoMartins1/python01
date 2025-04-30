a5ad9dc8aa121b7564680b476c9b8b567d1131a3
Armando Martins <armando.amds97@gmail.com>
2025-04-30 10:53:13 -0300

    função para calcular o valor total em um estoque

---
+++ atv03.py
@@ -0,0 +1,7 @@
+from biblioteca import valorEstoque
+
+nome = input("O nome do produto: ")
+estoque = int(input("Estoque: "))
+valor = float(input("preço do produto: "))
+
+valorEstoque(nome, estoque, valor)
--- biblioteca.py
+++ biblioteca.py
@@ -17,3 +17,7 @@ def contaVogal(texto):
         if texto[x] == "a" or texto[x] == "e" or texto[x] == "i" or texto[x] == "o" or texto[x] == "u":
             cont = cont + 1
     print(cont)
+
+def valorEstoque (nome_produto,estoque_produto,valor_unitario):
+    vTotal = estoque_produto*valor_unitario
+    print(f"{vTotal}")
