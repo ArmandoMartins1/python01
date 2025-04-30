27c6c4e142b6982b6281e5b755e9bd455dfc96c4
Armando Martins <armando.amds97@gmail.com>
2025-04-30 11:18:32 -0300

    funcao para verificar se o numero é pos,neg,zero

---
+++ atv04.py
@@ -0,0 +1,8 @@
+from biblioteca import verifica
+
+num = int(input("Digite um valor: "))
+olhar = verifica(num)
+print(olhar)
+
+
+print(verifica(5))
--- biblioteca.py
+++ biblioteca.py
@@ -21,3 +21,11 @@ def contaVogal(texto):
 def valorEstoque (nome_produto,estoque_produto,valor_unitario):
     vTotal = estoque_produto*valor_unitario
     return vTotal
+
+def verifica(num):
+    if num>0 :
+        return "P"
+    elif num<0:
+        return "N"
+    else:
+        return "Z"
